a96c5755b1b57210e500500998fa8dc61f49151f
jeonghlee12 <jl2374@cornell.edu>
2024-06-03 17:10:10 +0900

    Move GUI to different branch and reset development for backend use only

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-import recipeAppUI
-import database_setup
-
 def main():
-    app = QApplication([])
-
-    # establish DB connection
-    if not database_setup.connectDB():
-        recipeAppUI.DB_error("PostgreSQL connection error")
-    if not database_setup.initialize_DB():
-        recipeAppUI.DB_error("Failed to authenticate RecipeDB")
-
-    window = recipeAppUI.recipeApp()
-    window.show()
-
-    sys.exit(app.exec_())
-
+    pass
+    
 if __name__ == "__main__":
     main()
